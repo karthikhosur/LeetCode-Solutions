c3690c3ef01ae5a21004e611082e8c4e7e9c006f
karthikhosur <rheuzaki@gmail.com>
2022-02-08 22:05:27 -0600

    Time: 86 ms (25.00%), Space: 14.3 MB (96.00%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        letterDictionary = {}
+        
+        for char in s:
+            if char not in letterDictionary:
+                letterDictionary[char] = 1
+            else:
+                letterDictionary[char] += 1
+                
+        for char in t:
+            if char not in letterDictionary:
+                return False 
+            else:
+                if letterDictionary[char] == 1:
+                    del letterDictionary[char]
+                else:
+                    letterDictionary[char] -= 1
+                    
+        if letterDictionary != {}:
+            return False
+        
+        return True
